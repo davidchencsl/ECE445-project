8ae6055ded2babedf7b927f47e81a013125d4e8e
David Chen <davidchencsl@gmail.com>
2023-04-10 17:00:44 -0500

    sync

--- autolug/control/controller.py
+++ autolug/control/controller.py
@@ -3,9 +3,11 @@ from autolug.control.motor import Motor
 from autolug.control.encoder import Encoder
 import Jetson.GPIO as GPIO
 import time
+from smbus2 import SMBus
 
 ENC1 = 12
 ENC2 = 13
+
 
 def main():
     GPIO.setmode(GPIO.BOARD)
@@ -15,15 +17,21 @@ def main():
     encoder_left = Encoder(1, ENC1)
     encoder_right = Encoder(2, ENC2)
 
-    while True:
-        # set speed according to bounding box [m/s]
-        motor_left.set_speed(1)
-        motor_right.set_speed(1)
+    # set speed according to bounding box [m/s]
+    motor_left.set_speed(1)
+    motor_right.set_speed(1)
 
-        left_speed = encoder_left.get_speed()
-        right_speed = encoder_right.get_speed()
-        motor_left.update(left_speed)
-        motor_right.update(right_speed)
+    with SMBus(1) as bus:
+        while True:
+
+            left_speed = encoder_left.get_speed()
+            right_speed = encoder_right.get_speed()
+            motor_left_output = motor_left.update(left_speed)
+            motor_right_output = motor_right.update(right_speed)
+
+            bus.write_i2c_block_data(0x8, 0, [motor_left_output, motor_right_output])
+
+
 
 
 if __name__ == '__main__':
--- autolug/control/motor.py
+++ autolug/control/motor.py
@@ -1,6 +1,8 @@
 from simple_pid import PID
 import Jetson.GPIO as GPIO
-import smbus
+
+def clamp(val, mi, ma):
+    return max(min(val, ma), mi)
 
 def Motor():
     def __init__(self, id, bus, address):
@@ -8,7 +10,6 @@ def Motor():
         self.target_speed = 0
         self.current_speed = 0
         self.pid = PID(1, 0.1, 0.05, setpoint=0)
-        
     
     def set_speed(self, speed):
         self.target_speed = speed
@@ -18,6 +19,6 @@ def Motor():
         self.current_speed = current_speed
         output = self.pid(current_speed)
         print(f"Motor {self.id} pid: {output}")
-        # TODO: Send output through I2C to motor driver
+        return clamp(output, 0, 255)
 
 
